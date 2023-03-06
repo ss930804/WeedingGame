cbeea56768632e108615af8331d5c65f371aef84
ss930804 <93909610+ss930804@users.noreply.github.com>
2023-03-06 18:16:45 +0800

    20230306_Fixed

--- scripts/c3runtime.js
+++ scripts/c3runtime.js
@@ -4456,7 +4456,7 @@ self.C3_ExpressionFuncs = [
 		() => "GetPlayerData",
 		p => {
 			const v0 = p._GetNode(0).GetVar();
-			return () => (and("https://weedingg-e6f04-default-rtdb.asia-southeast1.firebasedatabase.app/LoadingData/", v0.GetValue()) + ".json");
+			return () => (("https://weedingg-e6f04-default-rtdb.asia-southeast1.firebasedatabase.app/LoadingData/" + v0.GetValue()) + ".json");
 		},
 		() => "MyList",
 		() => "AJAX事件",
@@ -4471,10 +4471,6 @@ self.C3_ExpressionFuncs = [
 		},
 		() => "null",
 		() => "AddNewPlayerData",
-		p => {
-			const v0 = p._GetNode(0).GetVar();
-			return () => (("https://weedingg-e6f04-default-rtdb.asia-southeast1.firebasedatabase.app/LoadingData/" + v0.GetValue()) + ".json");
-		},
 		p => {
 			const v0 = p._GetNode(0).GetVar();
 			return () => (("{\"Name\":\"" + v0.GetValue()) + "\"}");
@@ -4529,8 +4525,8 @@ self.C3_ExpressionFuncs = [
 			const v0 = p._GetNode(0).GetVar();
 			return () => (60 - v0.GetValue());
 		},
-		() => "<div id=\"fb-root\"></div>\n<script async defer crossorigin=\"anonymous\" src=\"https://connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v16.0\" nonce=\"6VozwxiT\"></script>\n<div class=\"fb-share-button\" data-href=\"https://www.kooco.co/\" data-layout=\"button\" data-size=\"large\" style=\"width=100% height=100%\"><a target=\"_blank\" href=\"https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse\" class=\"fb-xfbml-parse-ignore\">分享</a></div>",
-		() => "<div id=\"fb-root\"></div>\n<script async defer crossorigin=\"anonymous\" src=\"https://connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v16.0\" nonce=\"7XN23QtV\"></script>\n<div class=\"fb-share-button\" data-href=\"https://www.kooco.co/\" data-layout=\"button\" data-size=\"small\"><a target=\"_blank\" href=\"https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse\" class=\"fb-xfbml-parse-ignore\">分享</a></div>\n",
+		() => "<div id=\"fb-root\"></div>\n<script async defer crossorigin=\"anonymous\" src=\"https://connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v16.0\" nonce=\"6VozwxiT\"></script>\n<div id = \"Share\" class=\"fb-share-button\" data-href=\"https://www.kooco.co/\" data-layout=\"button\" data-size=\"large\" style=\"width=100% height=100%\"><a target=\"_blank\" href=\"https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse\" class=\"fb-xfbml-parse-ignore\">分享</a></div>",
+		() => "<div id=\"fb-root\"></div>\n<script async defer crossorigin=\"anonymous\" src=\"https://connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v16.0\" nonce=\"7XN23QtV\"></script>\n<div id = \"Share\" class=\"fb-share-button\" data-href=\"https://www.kooco.co/\" data-layout=\"button\" data-size=\"small\"><a target=\"_blank\" href=\"https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse\" class=\"fb-xfbml-parse-ignore\">分享</a></div>\n",
 		p => {
 			const v0 = p._GetNode(0).GetVar();
 			return () => (and("{\"Point\":\"", v0.GetValue()) + "\"}");
